4c5e793ef4f40420f87bb1be6835ff3e799e6f98
georgejogy <george.jogy01@gmail.com>
2021-10-10 17:47:01 +0530

    Details - header part - initial commit

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "../../common/header/Header";
 
-const Details = () => {
+const Details = (props) => {
 
     return <div>
-
+        <Header></Header>
     </div>
 }
 
